945165814d29b87ac071d0ae6a832cd36c917c73
tarbipyakurel21 <tarbipyakurel21@gmail.com>
2025-03-05 13:26:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 var express = require('express');
 var cors = require('cors');
+const multer =require('multer');
 require('dotenv').config()
 
 var app = express();
+const upload=multer({storage: multer.memoryStorage()});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
+app.post('/api/fileanalyse',upload.single("upfile"),async(req,res)=>{
 
+if(!req.file){
+  return res.status(400).json({error:"No file uploaded"});
+}
+
+res.json({
+
+  name:req.file.originalname,
+  type:req.file.mimetype,
+  size:req.file.size,
+});
+
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
